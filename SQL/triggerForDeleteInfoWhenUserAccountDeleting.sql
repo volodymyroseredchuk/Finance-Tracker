5cabd3bb4db6ec19eba133ca02618c24fafab755
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-04-04 15:44:19 +0300

    Create trigger to delete all user info when user deletes his account

---
+++ SQL/triggerForDeleteInfoWhenUserAccountDeleting.sql
@@ -0,0 +1,15 @@
+-- Trigger creation for database FinanceTracker
+
+IF OBJECT_ID (N'UserDeletedTrigger', N'TR') IS NOT NULL  
+    DROP TRIGGER UserDeletedTrigger;  
+GO
+
+CREATE TRIGGER UserDeletedTrigger
+ON [User_Account]
+FOR DELETE
+AS
+DECLARE @userID int
+SELECT @userID = ID FROM deleted
+
+DELETE FROM Profits WHERE UserID = @userID
+DELETE FROM Spending WHERE UserID = @userID
