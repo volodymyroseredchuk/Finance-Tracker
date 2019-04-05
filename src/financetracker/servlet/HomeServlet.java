5d68ddbf8d5c08b3623d4b2df0959f2e3ca2a661
Volodymyr Oseredchuk <oseredchuk.v@gmail.com>
2019-04-05 10:28:29 +0300

    User identification in login servlet done

--- src/financetracker/servlet/HomeServlet.java
+++ src/financetracker/servlet/HomeServlet.java
@@ -10,11 +10,16 @@ import javax.servlet.RequestDispatcher;
 
 @WebServlet(name = "HomeServlet", urlPatterns = {"/", "/home"})
 public class HomeServlet extends HttpServlet {
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
+    // show home page
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // forward to homeView.jsp - access to all view pages only by servlets
         RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/homeView.jsp");
         dispatcher.forward(request, response);
     }
--- src/financetracker/servlet/LoginServlet.java
+++ src/financetracker/servlet/LoginServlet.java
@@ -1,20 +1,93 @@
 package financetracker.servlet;
 
+import financetracker.beans.UserAccount;
+import financetracker.manager.DatabaseQueryManager;
+import financetracker.manager.TemporaryStoringManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
+
+    // this method will be called on click 'Submit' on login form
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        // get login info from form
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UserAccount user = null;
+        boolean hasError = false;
+        String errorString = null;
+
+        // check input data
+        if(userName == null || password == null || userName.isEmpty() || password.isEmpty())
+        {
+            // set type of error
+            hasError = true;
+            errorString = "Required user name and password!";
+        }
+        else
+        {
+            // check login credentials
+            Connection connection = TemporaryStoringManager.getStoredConnection(request);
+            try
+            {
+                // find user in database
+                user = DatabaseQueryManager.findUser(connection, userName);
+
+                if(user == null || !user.getPassword().equals(password))
+                {
+                    // user is not found or password is invalid
+                    hasError = true;
+                    errorString = "User name or password is invalid!";
+                }
+            }
+            catch(SQLException e)
+            {
+                // set type of error
+                e.printStackTrace();
+                hasError = true;
+                errorString = e.getMessage();
+            }
+        }
+
+        // if error - forward to login page
+        if(hasError)
+        {
+            // store user and error message in request attribute
+            user = new UserAccount(userName, password);
+            request.setAttribute("user", user);
+            request.setAttribute("errorString", errorString);
+
+            // forward to loginView.jsp
+            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
+            dispatcher.forward(request, response);
+        }
+        // if no error - forward to home page
+        {
+            // store logined user in Session
+            HttpSession session = request.getSession();
+            TemporaryStoringManager.storeLoginedUser(session, user);
+
+            // redirect to home page
+            response.sendRedirect(request.getContextPath() + "/home");
+        }
     }
 
+    // show login page
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // forward to loginView.jsp - access to all views only by servlets
         RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
         dispatcher.forward(request, response);
     }
